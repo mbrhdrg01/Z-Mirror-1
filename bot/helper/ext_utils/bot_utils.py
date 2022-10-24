407dda7a6f9296fcc679f66fd9e040ae4576911a
mbrhdrg01 <96500032+mbrhdrg01@users.noreply.github.com>
2022-10-25 01:44:02 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -163,7 +163,7 @@ T: {disk_t} | F: {disk_f}
 Working For: {currentTime}
 T-DL: {recv} | T-UL: {sent}
 
-Made with ❤️ by Dawn
+Made with ❤️ by ZKP143
 """
 
 #---Thanks for deleting my name ❤️ Appreciate it---#
